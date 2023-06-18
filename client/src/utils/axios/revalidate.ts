d58a6a4ca475e6db2a0ab80055df71cbd371caaf
Tonyba <anthonymessagesab@gmail.com>
2023-06-17 20:21:23 -0400

    se agrega revalidateChapter

--- client/src/utils/axios/revalidate.ts
+++ client/src/utils/axios/revalidate.ts
@@ -4,5 +4,10 @@ import { NEXT_API_URL } from "../constants";
 
 export const revalidateManga = (id: string) => {
   const path = encodeURIComponent(`content/${id}`);
-  return axios.get(`${NEXT_API_URL}/api/revalidate?path=${path}?secret=${process.env.NEXT_PUBLIC_REVALIDATION_TOKEN}`)
+  return axios.get(`${NEXT_API_URL}/api/revalidate?path=${path}?secret=${process.env.NEXT_PUBLIC_REVALIDATION_TOKEN}`);
+}
+
+export const revalidateChapter = (mangaId: number, capNumber: number) => {
+  const path = encodeURIComponent(`content/${mangaId}/${capNumber}`);
+  return axios.get(`${NEXT_API_URL}/api/revalidate?path=${path}?secret=${process.env.NEXT_PUBLIC_REVALIDATION_TOKEN}`);
 }
